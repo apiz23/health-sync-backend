879c22f2005a07823bd97eddf4907598f238915e
Hafizu <piz230601@gmail.com>
2025-03-25 21:45:31 +0800

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
-import * as cors from "cors";
 
 dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cors());
+
+  const corsOptions = {
+    origin: '*',
+    methods: 'GET,PUT,POST,DELETE',
+    credentials: true,
+    allowedHeaders: 'Content-Type,Authorization',
+  };
+
+  app.enableCors(corsOptions);
   await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
